55f2960e5ef9c90edb7e7cfb0715be169885ff91
ShreepriyaM <134845179+ShreepriyaM@users.noreply.github.com>
2023-05-28 18:23:29 +0530

    Create Message_pass.c

---
+++ Message_pass.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void main() 
+{
+    int count, num = 1;
+
+    printf("Enter the count of numbers to generate: ");
+    scanf("%d", &count);
+
+    while (count > 0) {
+        if (num % 2 == 0 && num % 3 == 0) {
+            printf("%d (pass)\n", num);
+        }
+        else if (num % 2 == 0) {
+            printf("%d (divisible by 2)\n", num);
+        }
+        else if (num % 3 == 0) {
+            printf("%d (divisible by 3)\n", num);
+        }
+
+        num++;
+        count--;
+    }
+
+}
+
+Output:
+
+Enter the count of numbers to generate: 10
+2 (divisible by 2)
+3 (divisible by 3)
+4 (divisible by 2)
+6 (pass)
+8 (divisible by 2)
+9 (divisible by 3)
+10 (divisible by 2)
