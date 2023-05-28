dd03cd747ba0a6cff35c5243ac68d8f3e2d372e1
ShreepriyaM <134845179+ShreepriyaM@users.noreply.github.com>
2023-05-28 19:32:56 +0530

    Create Squares.cpp

---
+++ Squares.cpp
@@ -0,0 +1,42 @@
+// Write a C++ program to print squares of numbers between 1 and 25
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 25; i++) {
+        int square = i * i;
+        cout << "Square of " << i << ": " << square << endl;
+    }
+
+    return 0;
+}
+
+Output:
+
+Square of 1: 1
+Square of 2: 4
+Square of 3: 9
+Square of 4: 16
+Square of 5: 25
+Square of 6: 36
+Square of 7: 49
+Square of 8: 64
+Square of 9: 81
+Square of 10: 100
+Square of 11: 121
+Square of 12: 144
+Square of 13: 169
+Square of 14: 196
+Square of 15: 225
+Square of 16: 256
+Square of 17: 289
+Square of 18: 324
+Square of 19: 361
+Square of 20: 400
+Square of 21: 441
+Square of 22: 484
+Square of 23: 529
+Square of 24: 576
+Square of 25: 625
