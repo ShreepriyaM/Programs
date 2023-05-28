f5151af0a9282dd681811fe4131bffb2dd335109
ShreepriyaM <134845179+ShreepriyaM@users.noreply.github.com>
2023-05-28 17:55:31 +0530

    Create DivisibleBy2.c

---
+++ DivisibleBy2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void main() 
+{
+    int number;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    if (number % 2 == 0) {
+        printf("%d is divisible by 2\n", number);
+    }
+    else {
+        printf("%d is not divisible by 2\n", number);
+    }
+
+}
+
+Output:
+
+Enter a number: 12
+12 is divisible by 2
+
+Enter a number: 9
+9 is not divisible by 2
