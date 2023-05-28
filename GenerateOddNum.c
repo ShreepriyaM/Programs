3b7001f03c557f49ea21d3ab856247b66e04feba
ShreepriyaM <134845179+ShreepriyaM@users.noreply.github.com>
2023-05-28 18:15:40 +0530

    Create GenerateOddNum.c

---
+++ GenerateOddNum.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void main() 
+{
+    int count, num = 1;
+
+    printf("Enter the count of odd numbers to generate: ");
+    scanf("%d", &count);
+
+    while (count > 0) {
+        printf("%d\n", num);
+        num += 2;
+        count--;
+    }
+
+}
+
+Output:
+
+Enter the count of odd numbers to generate: 10
+1
+3
+5
+7
+9
+11
+13
+15
+17
+19
