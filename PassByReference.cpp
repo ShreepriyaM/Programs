730c95a884cfacfaebfbd26faaa87381f8c1aaf8
ShreepriyaM <134845179+ShreepriyaM@users.noreply.github.com>
2023-05-28 20:01:50 +0530

    Create PassByReference.cpp

---
+++ PassByReference.cpp
@@ -0,0 +1,31 @@
+// Write a C++ program that subtracts two numbers using the concept of pass by reference
+
+#include <iostream>
+
+using namespace std;
+
+void subtractNumbers(int& num1, int& num2) {
+    num1 -= num2;
+}
+
+int main() {
+    int num1, num2;
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    subtractNumbers(num1, num2);
+
+    cout << "Result: " << num1 << endl;
+
+    return 0;
+}
+
+Output:
+
+Enter the first number: 96
+Enter the second number: 56
+Result: 40
